51dd2a10b02136dfe1f55fe1717c35cc192c311a
Андрей Волков <andrew.volkov.dev@gmail.com>
2020-11-12 02:36:53 +0200

    Wrote front page and made it responsive

---
+++ src/components/button/button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./button.css";
+
+const Button = () => {
+    return <button className="btn">Start</button>
+}
+
+export default Button;
--- src/components/pages/main-page.js
+++ src/components/pages/main-page.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import Button from "../button";
+
+import img from "../../img/hand.png";
+import "./main-page.css";
+
 const MainPage = () => {
-    return <div>Main page</div>;
+    return (
+        <div className="container flex">
+            <img className="main-image" alt="Hand and stars" src={img} />
+            <div className="heading-col"> 
+                <h1 className="main-heading">Who wants to be a millionaire?</h1>
+                <Button />
+            </div>    
+        </div>
+    );
 };
 
 export default MainPage;
